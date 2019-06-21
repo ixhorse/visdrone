8fb4036903173611937ea6a3563e9a4d4699093e
ixhorse <cyfhorse@gmail.com>
2019-06-21 15:17:36 +0800

    add new backbone

--- pytorch-deeplab-xception/modeling/backbone/__init__.py
+++ pytorch-deeplab-xception/modeling/backbone/__init__.py
@@ -1,4 +1,4 @@
-from modeling.backbone import resnet, xception, drn, mobilenet
+from modeling.backbone import resnet, xception, drn, mobilenet, mobilenetv3, shufflenetv2
 
 def build_backbone(backbone, output_stride, BatchNorm):
     if backbone == 'resnet':
@@ -9,5 +9,9 @@ def build_backbone(backbone, output_stride, BatchNorm):
         return drn.drn_d_54(BatchNorm)
     elif backbone == 'mobilenet':
         return mobilenet.MobileNetV2(output_stride, BatchNorm)
+    elif backbone == 'mobilenetv3':
+        return mobilenetv3.MobileNetV3_Small()
+    elif backbone == 'shufflenet':
+        return shufflenetv2.ShuffleNetV2()
     else:
         raise NotImplementedError
