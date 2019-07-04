d134121782376d71b08e76b411fa516a8322c808
ixhorse <cyfhorse@gmail.com>
2019-07-04 10:56:52 +0800

    visdrone submit code

--- tools/submit.py
+++ tools/submit.py
@@ -3,29 +3,39 @@ import os, sys
 import glob
 import json
 import shutil
+import argparse
 import numpy as np
-from path import Path
 import utils
 import pdb
 
-dbroot = Path.db_root_dir()
+from datasets import VisDrone
 
-src_traindir = dbroot + '/VisDrone2019-DET-train'
-src_valdir = dbroot + '/VisDrone2019-DET-val'
-src_testdir = dbroot + '/VisDrone2019-DET-test-challenge'
-
-voc_datadir = dbroot + '/detect_voc'
-voc_anno_dir = voc_datadir + '/Annotations'
-
-coco_datadir = dbroot + '/detect_coco'
-coco_image_dir = coco_datadir + '/images'
-coco_anno_dir = coco_datadir + '/annotations'
-
-val_anno = coco_anno_dir + '/instances_val.json'
-result_file =  '../mmdetection/visdrone/results.pkl.bbox.json'
-loc_file = voc_anno_dir + '/val_chip.json'
+def parse_args():
+    parser = argparse.ArgumentParser(description='VisDrone submit')
+    parser.add_argument('--split', type=str, default='val', help='split')
+    args = parser.parse_args()
+    return args
 
 if __name__ == '__main__':
+    args = parse_args()
+
+    dataset = VisDrone()
+    voc_datadir = dataset.detect_voc_dir
+    voc_anno_dir = voc_datadir + '/Annotations'
+
+    coco_datadir = dataset.detect_coco_dir
+    coco_image_dir = coco_datadir + '/images'
+    coco_anno_dir = coco_datadir + '/annotations'
+
+    val_anno = coco_anno_dir + '/instances_val.json'
+
+    if args.split == 'val':
+        result_file = '../mmdetection/visdrone/results.pkl.bbox.json'
+        loc_file = voc_anno_dir + '/val_chip.json'
+    elif args.split == 'test':
+        result_file = '../mmdetection/visdrone/results_region.json'
+        loc_file = voc_anno_dir + '/test_chip.json'
+
     with open(val_anno, 'r') as f:
         annos = json.load(f)
     with open(result_file, 'r') as f:
@@ -43,15 +53,21 @@ if __name__ == '__main__':
         cls_id = det['category_id'] + 1
         bbox = det['bbox']
         score = det['score']
-        loc = chip_loc[img_id2name[img_id]]
-        bbox = [bbox[0] + loc[0], bbox[1] + loc[1], bbox[2], bbox[3]]
-        img_name = '_'.join(img_id2name[img_id].split('_')[:-1]) + '.jpg'
+        if args.split == 'val':
+            loc = chip_loc[img_id2name[img_id]]
+            img_name = '_'.join(img_id2name[img_id].split('_')[:-1]) + '.jpg'
+            bbox = [bbox[0] + loc[0], bbox[1] + loc[1], bbox[2] - 1, bbox[3] - 1]
+        elif args.split == 'test':
+            loc = chip_loc[img_id[:-4]]['loc']
+            img_name = '_'.join(img_id.split('_')[:-1]) + '.jpg'
+            bbox = [bbox[0] + loc[0], bbox[1] + loc[1], bbox[2], bbox[3]]
+        
         if img_name in detecions:
             detecions[img_name].append(bbox + [score, cls_id])
         else:
             detecions[img_name] = [bbox + [score, cls_id]]
 
-    output_dir = 'DET_results-val'
+    output_dir = 'DET_results-%s' % args.split
     if os.path.exists(output_dir):
         shutil.rmtree(output_dir)
     os.mkdir(output_dir)
