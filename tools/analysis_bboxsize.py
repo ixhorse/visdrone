7a1a7fb4b1a1ebc2b5888849b59a175b13a81681
ixhorse <cyfhorse@gmail.com>
2019-10-19 11:42:32 +0800

    update vis codes

--- tools/analysis_bboxsize.py
+++ tools/analysis_bboxsize.py
@@ -2,26 +2,24 @@ import cv2
 import random
 import os, sys
 import glob
+import argparse
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import xml.etree.ElementTree as ET
-from path import Path
+
 import utils
+from datasets import get_dataset
 
 import pdb
 import traceback
 
-dbroot = Path.db_root_dir()
-
-src_traindir = dbroot + '/VisDrone2019-DET-train'
-src_valdir = dbroot + '/VisDrone2019-DET-val'
-src_testdir = dbroot + '/VisDrone2019-DET-test-challenge'
-
-voc_datadir = dbroot + '/detect_voc'
-image_dir = voc_datadir + '/JPEGImages'
-anno_dir = voc_datadir + '/Annotations'
-list_dir = voc_datadir + '/ImageSets/Main'
+def parse_args():
+    parser = argparse.ArgumentParser(description="analysis bbox size.")
+    parser.add_argument('dataset', type=str, default='VisDrone',
+                        choices=['VisDrone', 'HKB'], help='dataset name')
+    args = parser.parse_args()
+    return args
 
 def parse_xml(file):
     xml = ET.parse(file).getroot()
@@ -45,29 +43,32 @@ def parse_xml(file):
     return box_all, (width, height)
 
 if __name__ == '__main__':
+    args = parse_args()
+    dataset = get_dataset(args.dataset)
+
     origin_bboxsize = []
-    train_list = glob.glob(src_traindir + '/images/*.jpg')
+    train_list = dataset.get_imglist(split='train')
     for img_path in tqdm(train_list, ncols=80):
         img = cv2.imread(img_path)
         height, width = img.shape[:2]
-        label, _ = utils.get_box_label(img_path)
+        label, _ = dataset.get_gtbox(img_path)
         for box in label:
-            origin_bboxsize.append((box[2] - box[0]) / width)
+            origin_bboxsize.append((box[2] - box[0]) / width*1920)
 
-    after_bboxsize = []
-    with open(os.path.join(list_dir, 'train.txt'), 'r') as f:
-        train_list = [x.strip() for x in f.readlines()]
-    for img_id in tqdm(train_list, ncols=80):
-        label, size = parse_xml(os.path.join(anno_dir, img_id+'.xml'))
-        for box in label:
-            after_bboxsize.append((box[2] - box[0]) / size[0])
+    # after_bboxsize = []
+    # with open(os.path.join(list_dir, 'train.txt'), 'r') as f:
+    #     train_list = [x.strip() for x in f.readlines()]
+    # for img_id in tqdm(train_list, ncols=80):
+    #     label, size = parse_xml(os.path.join(anno_dir, img_id+'.xml'))
+    #     for box in label:
+    #         after_bboxsize.append((box[2] - box[0]) / size[0])
 
-    bins = [0., 0.025, 0.05, 0.075, 0.1, 0.125, 0.15, 0.175, 0.2, 0.3, 0.4, 0.5, 1]
-    plt.hist(origin_bboxsize, bins=bins, density=True, label='origin', alpha=0.4, histtype='step')
-    plt.hist(after_bboxsize, bins=bins, density=True, label='after', alpha=0.4, histtype='step')
+    bins = [x*5+20 for x in range(0, 20)]
+    plt.hist(origin_bboxsize, bins=bins, density=False, label='size', histtype='bar')
+    # plt.hist(after_bboxsize, bins=bins, density=True, label='after', histtype='step')
     plt.legend()
     plt.xlabel('size')
-    plt.ylabel('count')
+    plt.ylabel('nums')
 
     plt.title(u'size show')
 
--- tools/vis_mask.py
+++ tools/vis_mask.py
@@ -38,15 +38,13 @@ def _vis(img_path, dataset):
     img1 = img.copy()
     gt_box_list, _ = dataset.get_gtbox(img_path)
     for box in gt_box_list:
-        cv2.rectangle(img1, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 4)
-        cv2.putText(img1, str((box[2], box[3])), (box[0], box[3]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 4)
-    cv2.putText(img1, str((width, height)), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 4)
-    label_region_box, _ = utils.generate_box_from_mask(label_mask[:, :, 0])
-    print('box', gt_box_list)
-    print('region', label_region_box)
-    label_region_box = utils.resize_box(label_region_box, (40, 30), (width, height))
-    for box in label_region_box:
-        cv2.rectangle(img1, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 5)
+        cv2.rectangle(img1, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 2)
+    #     cv2.putText(img1, str((box[2], box[3])), (box[0], box[3]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 4)
+    # cv2.putText(img1, str((width, height)), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 4)
+    # label_region_box, _ = utils.generate_box_from_mask(label_mask[:, :, 0])
+    # label_region_box = utils.resize_box(label_region_box, (40, 30), (width, height))
+    # for box in label_region_box:
+    #     cv2.rectangle(img1, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 5)
 
     # region box
     img2 = img.copy()
@@ -54,7 +52,7 @@ def _vis(img_path, dataset):
     region_box, contours = utils.generate_box_from_mask(pred_mask[:, :, 0])
     resize_region_box = utils.resize_box(region_box, (mask_w, mask_h), (width, height))
     for box in resize_region_box:
-        cv2.rectangle(img2, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 5)
+        cv2.rectangle(img2, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 2)
 
     # region postprocess
     img3 = img.copy()
@@ -62,20 +60,30 @@ def _vis(img_path, dataset):
     resize_region_box = utils.resize_box(new_regions, (mask_w, mask_h), (width, height))
     # new_regions = utils.generate_crop_region(resize_region_box, (width, height))
     for box in resize_region_box:
-        cv2.rectangle(img3, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 5)
+        cv2.rectangle(img3, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 2)
 
     img4 = img.copy()
     # resize_region_box = utils.resize_box(temp, (mask_w, mask_h), (width, height))
     new_regions = utils.generate_crop_region(resize_region_box, (width, height))
     for box in new_regions:
-        cv2.rectangle(img4, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 10)
+        cv2.rectangle(img4, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 2)
 
-    plt.subplot(1, 1, 1); plt.imshow(img1[:, :, [2,1,0]])
-    # plt.subplot(2, 3, 2); plt.imshow(img2[:, :, [2,1,0]])
-    # plt.subplot(2, 3, 3); plt.imshow(img3[:, :, [2,1,0]])
-    # plt.subplot(2, 3, 4); plt.imshow(label_mask[:, :, [2,1,0]])
-    # plt.subplot(2, 3, 5); plt.imshow(pred_mask[:, :, [2,1,0]])
-    # plt.subplot(2, 3, 6); plt.imshow(img4[:, :, [2,1,0]])
+    plt.subplot(2, 3, 1); plt.imshow(img1[:, :, [2,1,0]])
+    plt.subplot(2, 3, 2); plt.imshow(img2[:, :, [2,1,0]])
+    plt.subplot(2, 3, 3); plt.imshow(img3[:, :, [2,1,0]])
+    plt.subplot(2, 3, 4); plt.imshow(label_mask[:, :, [2,1,0]])
+    plt.subplot(2, 3, 5); plt.imshow(pred_mask[:, :, [2,1,0]])
+    plt.subplot(2, 3, 6); plt.imshow(img4[:, :, [2,1,0]])
+
+    dirname = os.path.join(pred_mask_dir, os.path.pardir)
+    cv2.imwrite(os.path.join(dirname, 'virtualization', 'image.jpg'), img)
+    cv2.imwrite(os.path.join(dirname, 'virtualization', 'bbox.jpg'), img1)
+    cv2.imwrite(os.path.join(dirname, 'virtualization', 'brec.jpg'), img2)
+    cv2.imwrite(os.path.join(dirname, 'virtualization', 'post_process.jpg'), img3)
+    cv2.imwrite(os.path.join(dirname, 'virtualization', 'label_mask.jpg'), label_mask)
+    cv2.imwrite(os.path.join(dirname, 'virtualization', 'pred_mask.jpg'), pred_mask)
+    cv2.imwrite(os.path.join(dirname, 'virtualization', 'result.jpg'), img4)
+
     
     plt.show()
     cv2.waitKey(0)
@@ -89,9 +97,9 @@ if __name__ == '__main__':
     segmentation_dir = dest_datadir + '/SegmentationClass'
     list_folder = dest_datadir + '/ImageSets'
 
-    pred_mask_dir = '../pytorch-deeplab-xception/run/mask-%sval' % args.dataset.lower()
+    pred_mask_dir = '../pytorch-deeplab-xception/run/mask-%s-val' % args.dataset.lower()
     val_list = dataset.get_imglist('val')
 
-    for img_path in val_list:
+    for img_path in val_list[2:]:
         _vis(img_path, dataset)
     
